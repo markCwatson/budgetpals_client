c4dafc968386fdfc6007c59004a75dde0c03b279
Mark Watson <mwatson@prosaris.ca>
2023-08-23 17:59:38 -0300

    screens/auth

---
+++ lib/screens/auth.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: const Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SignInText('email'),
+            SignInText('password'),
+            SignInButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SignInText extends StatelessWidget {
+  const SignInText(this.text, {Key? key}) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          hintText: 'Enter your $text',
+        ),
+        validator: (String? value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter some text';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
+
+class SignInButton extends StatelessWidget {
+  const SignInButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: const Text('Sign in'),
+      ),
+    );
+  }
+}
