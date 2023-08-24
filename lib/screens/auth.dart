7a05b3fb6039ed7ee2dab726f4c1f80e60c44ff4
Mark Watson <mwatson@prosaris.ca>
2023-08-23 21:06:36 -0300

    validate form fields

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -8,25 +8,28 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Card(
-              margin: EdgeInsets.all(24.0),
+              margin: const EdgeInsets.all(24.0),
               child: SingleChildScrollView(
                 child: Form(
+                  key: _formKey,
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      EmailField(),
-                      PasswordField(),
-                      Buttons(),
+                      const EmailField(),
+                      const PasswordField(),
+                      Buttons(formKey: _formKey),
                     ],
                   ),
                 ),
@@ -53,6 +56,12 @@ class EmailField extends StatelessWidget {
         keyboardType: TextInputType.emailAddress,
         autocorrect: false,
         textCapitalization: TextCapitalization.none,
+        validator: (value) {
+          if (value == null || value.trim().isEmpty || !value.contains('@')) {
+            return 'Please enter a valid email address.';
+          }
+          return null;
+        },
       ),
     );
   }
@@ -70,13 +79,21 @@ class PasswordField extends StatelessWidget {
           labelText: 'Enter Password',
         ),
         obscureText: true,
+        validator: (value) {
+          if (value == null || value.length < 6) {
+            return 'Please enter a password with at least 6 characters.';
+          }
+          return null;
+        },
       ),
     );
   }
 }
 
 class Buttons extends StatefulWidget {
-  const Buttons({super.key});
+  const Buttons({super.key, required this.formKey});
+
+  final GlobalKey<FormState> formKey;
 
   @override
   State<Buttons> createState() => _ButtonState();
@@ -85,6 +102,10 @@ class Buttons extends StatefulWidget {
 class _ButtonState extends State<Buttons> {
   var _isLogin = false;
 
+  void _onSubmit() {
+    widget.formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -92,11 +113,11 @@ class _ButtonState extends State<Buttons> {
       child: Column(
         children: [
           ElevatedButton(
+            onPressed: _onSubmit,
             style: ElevatedButton.styleFrom(
               backgroundColor: Theme.of(context).colorScheme.secondary,
               foregroundColor: Colors.white,
             ),
-            onPressed: () {},
             child: Text(_isLogin ? 'Log in' : 'Sign up'),
           ),
           const SizedBox(height: 8.0),
